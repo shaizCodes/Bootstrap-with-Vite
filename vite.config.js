9a4a20a1757850a1c53c0775322b17999498b835
Shahzad <shaizcodes@gmail.com>
2025-12-04 01:12:26 +0500

    configure Vite build, and silence SCSS deprecation warnings

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,22 @@ import { resolve } from 'path'
 
 export default defineConfig({
     root: resolve(__dirname, './src'),
+    build: {
+        outDir: '../dist'
+    },
     server: {
         port: 8080,
         open: true
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: [
+                    'import',
+                    'color-functions',
+                    'global-builtin'
+                ]
+            }
+        }
     }
 });
